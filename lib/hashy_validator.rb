14ff736b9166079b93a154b24b3758415b79f95e
Alexandro Castro <10711649+AlexcastroDev@users.noreply.github.com>
2024-01-13 18:27:25 +0000

    chore: code quality and rubocop (#16)
    * chore: code quality and rubocop
    
    * chore: validate pr

--- lib/hashy_validator.rb
+++ lib/hashy_validator.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'active_record'
 require 'hash_validator'
 require_relative './hashy_validator/hashy_value_validator'
--- lib/hashy_validator/hashy_value_validator.rb
+++ lib/hashy_validator/hashy_value_validator.rb
@@ -1,42 +1,34 @@
-class HashyValueValidator
-    def initialize(value)
-      @value = value.blank? ? [] : value
-      @valid = true
-      @reason = nil
-  
-      check_parse_value
-      check_is_array
-      
-      @value
-    end
-  
-    def is_valid
-      @valid
-    end
-  
-    def value
-      @value
-    end
+# frozen_string_literal: true
 
-    def reason
-      @reason
-    end
-  
-    private
-  
-    def check_parse_value
-      begin
-        @value = JSON.parse(@value) if @value.is_a?(String)
-      rescue JSON::ParserError
-        @valid = false
-        @reason = :invalid
-      end
-    end
-  
-    def check_is_array
-      unless @value.is_a?(Array)
-        @valid = false
-        @reason = :not_an_array
-      end
-    end
+class HashyValueValidator
+  def initialize(value)
+    @value = value.blank? ? [] : value
+    @valid = true
+    @reason = nil
+
+    check_parse_value
+    check_is_array
   end
+
+  def valid?
+    @valid
+  end
+
+  attr_reader :value, :reason
+
+private
+
+  def check_parse_value
+    @value = JSON.parse(@value) if @value.is_a?(String)
+  rescue JSON::ParserError
+    @valid = false
+    @reason = :invalid
+  end
+
+  def check_is_array
+    return if @value.is_a?(Array)
+
+    @valid = false
+    @reason = :not_an_array
+  end
+end
