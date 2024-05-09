fb142a45b575e53150d2e7a2f297cfbc5b6a5b8c
Alexandro Castro <10711649+AlexcastroDev@users.noreply.github.com>
2024-05-09 15:52:56 +0100

    chore: add more specs and add shopify-rubocop (#17)
    * chore: code quality and rubocop
    
    * chore: validate pr
    
    * chore: start fixing
    
    * chore: add more specs

--- test/validation_sqlite_test.rb
+++ test/validation_sqlite_test.rb
@@ -1,7 +1,9 @@
-require 'simplecov'
+# frozen_string_literal: true
+
+require "simplecov"
 SimpleCov.start
-require 'minitest/autorun'
-require_relative '../lib/hashy_validator'
+require "minitest/autorun"
+require_relative "../lib/hashy_validator"
 
 # Create database
 conn = { adapter: "sqlite3", database: ":memory:" }
@@ -9,130 +11,164 @@ ActiveRecord::Base.establish_connection(conn)
 
 # Schema
 ActiveRecord::Schema.define do
-    create_table :profiles do |t|
-        t.string :name
-        t.string :notifications
-        
-        # @Todo: use Postgres
-        # Also check if i really need to create test with pg integration
-        # t.jsonb :notifications
-    end
-    create_table :products do |t|
-      t.string :name
-      t.string :discount_by_quantity
+  create_table :profiles do |t|
+    t.string(:name)
+    t.string(:notifications)
+
+    # @Todo: use Postgres
+    # Also check if i really need to create test with pg integration
+    # t.jsonb :notifications
+  end
+  create_table :products do |t|
+    t.string(:name)
+    t.string(:discount_by_quantity)
+  end
+
+  create_table :items do |t|
+    t.string(:name)
+    t.string(:metas)
   end
 end
 
-
 # Model
 class Profile < ActiveRecord::Base
-    validates :name, presence: true
-    validates :notifications, hashy_array: {
-        type: HashValidator.multiple('required', 'string', 'unique'),
-    }
+  validates :name, presence: true
+  validates :notifications, hashy_array: {
+    type: HashValidator.multiple("required", "string", "unique"),
+  }
 end
 
 class Product < ActiveRecord::Base
   validates :name, presence: true
   validates :discount_by_quantity, hashy_array: {
-    uuid: 'unique',
-    quantity: HashValidator.multiple('required', 'numeric'),
-    price: HashValidator.multiple('required', 'numeric'),
-    active: HashValidator.multiple('required', 'boolean'),
+    uuid: "unique",
+    quantity: HashValidator.multiple("required", "numeric"),
+    price: HashValidator.multiple("required", "numeric"),
+    active: HashValidator.multiple("required", "boolean"),
   }
+end
+
+class Item < ActiveRecord::Base
+  validates :name, presence: true
+  validates :metas,
+    hashy_array: {
+      active: HashValidator.multiple("required", "boolean"),
+    },
+    if: ->(item) { self.quantity > 1 }
+
+  attribute :quantity, :integer
 end
 
 # I am using JSON.generate is because sqlite3 does not have the jsonb column type
 class HashyArrayValidationTest < Minitest::Test
-    def test_valid_hashy_returns_success
-        profile = Profile.new(name: 'John Doe', notifications: JSON.generate(
-          [
-               {
-                 type: 'something'
-               }
-            ]
-        ))
-
-        assert profile.valid?
-    end
-    def test_valid_numeric_hashy_returns_success
-      product = Product.new(name: 'IPhone', discount_by_quantity: JSON.generate(
-        [
-             {
-               uuid: '1a2b3c4d5e6f7g8h9i0j',
-               quantity: 3,
-               price: 100,
-               active: true
-             }
-          ]
-      ))
-
-      assert product.valid?
-  end
-  def test_invalid_numeric_hashy_returns_error
-    product = Product.new(name: 'IPhone', discount_by_quantity: JSON.generate(
-    [
-          {
-            uuid: '1a2b3c4d5e6f7g8h9i0j',
-            quantity: 'something',
-            price: 10,
-            active: false
-          },
-          {
-            uuid: '1a2b3c4d5e6f7g8h9i0j',
-            quantity: 'something 2',
-            price: 10,
-            active: false
-          }
-      ]
+  def test_valid_hashy_returns_success
+    profile = Profile.new(name: "John Doe", notifications: JSON.generate(
+      [
+        {
+          type: "something",
+        },
+      ],
     ))
 
-    refute product.valid?
+    assert(profile.valid?)
   end
+
+  def test_valid_numeric_hashy_returns_success
+    product = Product.new(name: "IPhone", discount_by_quantity: JSON.generate(
+      [
+        {
+          uuid: "1a2b3c4d5e6f7g8h9i0j",
+          quantity: 3,
+          price: 100,
+          active: true,
+        },
+      ],
+    ))
+
+    assert(product.valid?)
+  end
+
+  def test_valid_hashy_with_condition_returns_success
+    item = Item.new(name: "Go Pro", quantity: 1, metas: JSON.generate([{}]))
+
+    assert(item.valid?)
+  end
+
+  def test_valid_hashy_with_condition_returns_fail
+    item = Item.new(name: "Go Pro", quantity: 2, metas: JSON.generate([{}]))
+
+    refute(item.valid?)
+  end
+
+  def test_invalid_numeric_hashy_returns_error
+    product = Product.new(name: "IPhone", discount_by_quantity: JSON.generate(
+      [
+        {
+          uuid: "1a2b3c4d5e6f7g8h9i0j",
+          quantity: "something",
+          price: 10,
+          active: false,
+        },
+        {
+          uuid: "1a2b3c4d5e6f7g8h9i0j",
+          quantity: "something 2",
+          price: 10,
+          active: false,
+        },
+      ],
+    ))
+
+    refute(product.valid?)
+  end
+
   def test_invalid_hash_and_returns_error
-    product = Product.new(name: 'IPhone', discount_by_quantity: 'abacate')
+    product = Product.new(name: "IPhone", discount_by_quantity: "abacate")
 
-    refute product.valid?
+    refute(product.valid?)
   end
-    def test_invalid_not_unique_hashy_returns_error
-        profile = Profile.new(name: 'John Doe', notifications: JSON.generate(
-          [
-                {
-                  type: 'something'
-                },
-                {
-                  type: 'something'
-                },
-            ]
-        ))
 
-        refute profile.valid?
-    end
-    def test_invalid_not_array_hashy_returns_error
-        profile = Profile.new(name: 'John Doe', notifications: JSON.generate(
-            { type: 'something' }
-        ))
+  def test_invalid_not_unique_hashy_returns_error
+    profile = Profile.new(name: "John Doe", notifications: JSON.generate(
+      [
+        {
+          type: "something",
+        },
+        {
+          type: "something",
+        },
+      ],
+    ))
 
-        refute profile.valid?
-    end
-    def test_invalid_wrong_type_hashy_returns_error
-        profile = Profile.new(name: 'John Doe', notifications: JSON.generate(
-          [
-            {
-              type: 1
-            },
-          ]
-        ))
+    refute(profile.valid?)
+  end
 
-        refute profile.valid?
-    end
-    def test_invalid_hashy_returns_error
-        profile = Profile.new(name: 'John Doe', notifications: [
-          {
-            test: 'something'
-          }
-        ])
+  def test_invalid_not_array_hashy_returns_error
+    profile = Profile.new(name: "John Doe", notifications: JSON.generate(
+      { type: "something" },
+    ))
 
-        refute profile.valid?
-    end
+    refute(profile.valid?)
+  end
+
+  def test_invalid_wrong_type_hashy_returns_error
+    profile = Profile.new(name: "John Doe", notifications: JSON.generate(
+      [
+        {
+          type: 1,
+        },
+      ],
+    ))
+
+    refute(profile.valid?)
+  end
+
+  def test_invalid_hashy_returns_error
+    profile = Profile.new(name: "John Doe", notifications: [
+      {
+        test: "something",
+      },
+    ])
+
+    refute(profile.valid?)
+  end
 end
