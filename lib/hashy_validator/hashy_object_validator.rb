35e1ccf1cff9d95df069961c8ac1b3fec6d23645
Alexandro Castro <10711649+AlexcastroDev@users.noreply.github.com>
2024-09-05 11:06:09 +0100

    fix: add hashy_object

---
+++ lib/hashy_validator/hashy_object_validator.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+class HashyObjectValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    instance_value = HashyValueValidator.new(value, options)
+
+    # Do not validate empty hash
+    return if instance_value.value.keys.empty?
+
+    unless instance_value.valid?
+      record.errors.add(attribute, instance_value.reason)
+      return false
+    end
+
+    if instance_value.value.is_a?(Array)
+      record.errors.add(attribute, :not_an_object)
+      return false
+    end
+
+    # force all array entries to have string keys
+    # discard keys that do not have validators
+    value = instance_value.value.stringify_keys.slice(*instance_value.validations.keys)
+
+    # we validate each object in the array
+    # if boolean found as any of the validations we force value to boolean - if present
+    instance_value.boolean_attrs.each do |boolean_attr|
+      value[boolean_attr] = HashyValueValidator.get_boolean_value(t[boolean_attr]) if value.key?(boolean_attr)
+    end
+
+    # keep track of unique values and add error if needed
+    instance_value.unique_attrs.each_key do |unique_attr|
+      if instance_value.unique_attrs[unique_attr].include?(value[unique_attr])
+        record.errors.add(attribute, "'#{unique_attr}' not unique")
+      else
+        instance_value.unique_attrs[unique_attr] << t[unique_attr]
+      end
+    end
+
+    # use default hash validator
+    validator = HashValidator.validate(value, instance_value.validations)
+    unless validator.valid?
+      validator.errors.each { |k, v| record.errors.add(attribute, "'#{k}' #{v}") }
+    end
+
+    # update the value even if errors found
+    # we use send write param so we also support attr_accessor attributes
+    record.send("#{attribute}=", value)
+  end
+end
---
+++ test/model/samples.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class Profile < ActiveRecord::Base
+  validates :name, presence: true
+  validates :notifications, hashy_array: {
+    type: HashValidator.multiple("string", "unique"),
+    header: HashValidator::Validations::Optional.new("string"),
+  }
+end
+
+class Product < ActiveRecord::Base
+  validates :name, presence: true
+  validates :discount_by_quantity, hashy_array: {
+    uuid: "unique",
+    quantity: HashValidator.multiple("numeric"),
+    price: HashValidator.multiple("numeric"),
+    active: HashValidator.multiple("boolean"),
+  }
+end
+
+class Item < ActiveRecord::Base
+  validates :name, presence: true
+  validates :metas,
+    hashy_array: {
+      active: HashValidator.multiple("boolean"),
+    },
+    if: ->(_item) { quantity > 1 }
+
+  attribute :quantity, :integer
+end
+
+class Customer < ActiveRecord::Base
+  validates :age, presence: true, numericality: { only_integer: true, greater_than: 20, less_than: 100 }
+  validates :custom, hashy_object: {
+    name: HashValidator::Validations::Optional.new("string"),
+    quantity: HashValidator.multiple("numeric"),
+  }
+end
